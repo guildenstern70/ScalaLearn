b9c78e66ae702c98698c8f6cbe77aa39f7c1d2e6
Alessio <alessiosaltarin@gmail.com>
2013-10-25 13:00:20 +0200

    First class functions v2

--- ScalaLearn/src/main/scala/scalalearn/FirstClassFunctions.scala
+++ ScalaLearn/src/main/scala/scalalearn/FirstClassFunctions.scala
@@ -21,20 +21,28 @@ package scalalearn
 
 object FirstClassFunctions extends LearningModule {
 
-    // AreaDelCerchio is a function that takes one parameter of type Double
+    // circleArea and squareArea are functions that takes one parameter of type Double
     // and returns a Double
-    val areaDelCerchio = (raggio: Double) => { raggio * raggio * Math.PI }
+    val circleArea = (radius: Double) => { radius * radius * Math.PI }
+    val squareArea = (side: Double) => { side * side } 
     
-    // RemString is a function that takes no parameters and returns a String
-    val remString = () => { "REM" }
+    
+    // A first class function is a function that takes in input another function
+    def firstClassFunction( func: Double => Double, v: Double ) : String = {
+        
+        val result = func(v)
+        result.toString();
+        
+    }
     
 
     def printOutput() {
 
         val test = new ResultsPrinter("FIRST CLASS FUNCTION")
-        val result = areaDelCerchio(10).toString
-        test.addResult("> Circle area with radius = 10 => %s ".format(result))
-        test.addResult(remString + ": trial")
+        val result1 = this.firstClassFunction(circleArea, 10.0)
+        val result2 = this.firstClassFunction(squareArea, 10.0)
+        test.addResult("> Circle area with radius = 10 => %s ".format(result1))
+        test.addResult("> Square area with side = 10 => %s ".format(result2))
         test.print
 
     }
