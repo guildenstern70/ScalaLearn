2eb1e2912bcaafbc758c447ed0c45d956ffb4f5d
Alessio Saltarin <alessiosaltarin@gmail.com>
2014-09-17 18:17:45 +0200

    For enhancements

--- ScalaLearn/src/main/scala/scalalearn/ForAndWhile.scala
+++ ScalaLearn/src/main/scala/scalalearn/ForAndWhile.scala
@@ -22,53 +22,62 @@ package scalalearn
 import scala.collection.mutable.ListBuffer
 
 object ForAndWhile extends LearningModule {
-     
-    val someNumbers = Array(1,2,3,4,5)
-    val results1 = new ListBuffer[Int]()
-    val results2 = new ListBuffer[Int]()
-    val results3 = new ListBuffer[Int]()
-    val results4 = new ListBuffer[Int]()
-    val results5 = new ListBuffer[Int]()
-    
-    // Iterate over a list
-    for (j <- someNumbers) {
-        results1.append(j)
-    }
-    
-    // Indices: get item at without using a var...
-    for (k <- someNumbers.indices) {
-        results2.append(someNumbers(k))
-    }
-    
-    // Classic for
-    for (i <- 1 to 4) {
-    	results3.append(i)
-    }
-    
-    // Filtering
-    for (
-            i <- 1 until 20
-            if (i % 2 == 0)
-    	) {
-        results4.append(i)
-    }
-    
-    // While
-    var a = 150
-    var b = 40
-    while (a != 0) {
-    	val temp = a
-    	a = b % a
-    	b = temp
-    	results5.append(b)
-    }
-            
-    def printOutput() {
-        new ResultsPrinter("FOR 1/3").printDetails(results1.toList.iterator)
-        new ResultsPrinter("FOR 2/3").printDetails(results2.toList.iterator)
-        new ResultsPrinter("FOR 3/3").printDetails(results3.toList.iterator)
-        new ResultsPrinter("FOR 3/3").printDetails(results4.toList.iterator)
-        new ResultsPrinter("WHILE").printDetails(results5.toList.iterator)
-    }
+
+  val someNumbers = Array(1, 2, 3, 4, 5)
+  var results = new ListBuffer[Int]()
+  var testName: String = "";
+
+  // Iterate over a list
+  // var j; can be omitted!
+  for (j <- someNumbers) {
+    results.append(j)
+  }
+  this.printOutput("FOR #1");
+
+  // Indices: get item at without using a var...
+  for (k <- someNumbers.indices) {
+    results.append(someNumbers(k))
+  }
+  this.printOutput("FOR #2");
+
+  // Classic for
+  for (i <- 1 to 4) {
+    results.append(i)
+  }
+  this.printOutput("FOR #3");
+
+  // Yielding
+  val ints = for (a <- someNumbers) yield (a * 2);
+  this.results = ints.to[ListBuffer]; // !! Brilliant conversion!
+  this.printOutput("YIELD");
+
+  // Filtering
+  for (
+    i <- 1 until 20 if (i % 2 == 0)
+  ) {
+    results.append(i)
+  }
+  this.printOutput("FILTER");
+
+  // While
+  var a = 150
+  var b = 40
+  while (a != 0) {
+    val temp = a
+    a = b % a
+    b = temp
+    results.append(b)
+  }
+  this.printOutput("WHILE");
+  
+  def printOutput() {
+    new ResultsPrinter(this.testName + " -> ").printDetails(this.results.toList.iterator);
+    this.results = new ListBuffer[Int]();
+  }
+
+  def printOutput(testName: String) {
+    this.testName = testName;
+    this.printOutput();
+  }
 
 }
